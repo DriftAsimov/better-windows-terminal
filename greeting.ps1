a1d751ccf550ca7058ec25abd1ddc3224060e2b2
DriftAsimov <65854695+DriftAsimov@users.noreply.github.com>
2021-08-13 09:41:22 +0530

    Added greeting.ps1

---
+++ greeting.ps1
@@ -0,0 +1,23 @@
+Write-Color " "
+
+$script:time = Get-Date -Format "HH"
+$time = $time -as [int]
+
+$script:msg = "$($env:username)-kun!"
+
+if ($time -ge 12) {
+    Write-Color -Text "Konnichiwa $($msg)" -Color DarkAqua
+}
+elseif ($time -ge 16) {
+    Write-Color -Text "Konbanwa $($msg)" -Color Cyan
+}
+else {
+    Write-Color -Text "Ohayo $($msg)" -Color DarkYellow
+}
+
+Write-Color " "
+
+function My
+{
+    cd "E:/Gate of Steiner/Code/"
+}
